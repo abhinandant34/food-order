0b1f1f8329bf06d1a276fffdf64167ea26a8051c
Abhinandan Thakur <73791479+abhinandant34@users.noreply.github.com>
2023-10-26 09:19:57 +0530

    -Header added

---
+++ src/app/shared/model/Food.ts
@@ -0,0 +1,12 @@
+export class Food
+{
+    id!:string;
+    name!:string;
+    price!:number;
+    tag?:string[];
+    favourite!:boolean;
+    stars!:number;
+    imageUrl!:string;
+    origins!:string[];
+    cookTime!:string;
+}
---
+++ src/data.ts
@@ -0,0 +1,93 @@
+import { Food } from "./app/shared/model/Food";
+
+export const sample-food:Food[]=[
+    {
+        id:'1',
+        name:'Tomato Pizza',
+        price:20,
+        cookTime: '45-50',
+        favorite:false,
+        origins: ['indian'],
+        starts: 4.0,
+        imageUrl:'/assets/food-1.jpg',
+        tags: ['FastFood','Pizza','Lunch']
+    },
+    {
+        id:'2',
+        name:'Meatball',
+        price:30,
+        cookTime: '25-30',
+        favorite:true,
+        origins: ['indian','middle east','china'],
+        starts: 4.7,
+        imageUrl:'/assets/food-2.jpg',
+        tags: ['SlowFood','Lunch']
+    },
+    {
+        id:'3',
+        name:'Hamburger',
+        price:10,
+        cookTime: '10-15',
+        favorite:false,
+        origins: ['germany','us',],
+        starts: 3.5,
+        imageUrl:'/assets/food-3.jpg',
+        tags: ['FastFood','Hamburger']
+    },
+    {
+        id:'4',
+        name:'Fried Potatoes',
+        price:5,
+        cookTime: '15-20',
+        favorite:true,
+        origins: ['belgium','france'],
+        starts: 3.3,
+        imageUrl:'/assets/food-4.jpg',
+        tags: ['FastFood','Fry']
+    },
+    {
+        id:'5',
+        name:'Chicken Soup',
+        price:11,
+        cookTime: '45-50',
+        favorite:false,
+        origins: ['indian','asia'],
+        starts: 3.0,
+        imageUrl:'/assets/food-5.jpg',
+        tags: ['FastFood','Pizza','Lunch']
+    },
+    {
+        id:'6',
+        name:'Vegetable Pizza',
+        price:30,
+        cookTime: '40-50',
+        favorite:true,
+        origins: ['italy'],
+        starts: 4.0,
+        imageUrl:'/assets/food-6.jpg',
+        tags: ['FastFood','Pizza','Lunch']
+    },
+    {
+        id:'7',
+        name:'Spicy Cheese Burger',
+        price:12,
+        cookTime: '20-30',
+        favorite:false,
+        origins: ['indian'],
+        starts: 3.5,
+        imageUrl:'/assets/food-3.jpg',
+        tags: ['FastFood','Burger','Lunch']
+    },
+    {
+        id:'8',
+        name:'Vegetable Magento Pizza',
+        price:5,
+        cookTime: '45-50',
+        favorite:false,
+        origins: ['indian'],
+        starts: 4.0,
+        imageUrl:'/assets/food-4.jpg',
+        tags: ['FastFood','Pizza','Lunch']
+    }
+
+]
